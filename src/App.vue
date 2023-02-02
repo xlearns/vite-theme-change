2c0d62715a6e5292eae9a4b16ffa2b0cae02294e
xlearns <2447684095@qq.com>
2023-02-02 09:57:20 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { themeChange } from "theme-change";
+
+const themes = ["default", "dark", "pink"];
+
+onMounted(() => {
+	themeChange(false);
+});
+</script>
+
+<template>
+	<div>
+		<button data-set-theme="">default</button>
+		<button data-set-theme="dark">dark</button>
+		<button data-set-theme="pink">pink</button>
+	</div>
+
+	<div
+		style="
+			height: 500px;
+			background: #fff;
+			margin: 20px;
+			color: var(--my-color);
+		"
+	>
+		hello world
+	</div>
+</template>
